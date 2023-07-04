1f66e3f6028e6fc3d15302b1972edf195907866c
Alberto Glez Suárez <albertoglez@MacBook-Pro-de-Alberto-2.local>
2023-07-04 12:04:31 +0200

    Prueba Composite API

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@ import Cards from './components/Cards.vue';
 </script>
 
 <template>
-        <RouterLink to="/">Home</RouterLink>
-        <Cards/>
-  <RouterView />
+  <RouterLink to="/">Home</RouterLink>
+        <div class="container">
+          <Cards/>
+        </div>
+        <RouterView />
 </template>
 
 <style scoped>
-
+.container {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+}
 </style>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,34 +1,33 @@
 <script>
+import { ref, onMounted } from 'vue';
 import Cards from '../services/tarot-services';
 
-export default{
-    name:'CardComponent',
-    data() {
-    return {
-        state: []
-    };
-  },
-  async mounted() {
-    this.state = await Cards();
-  },
+export default {
+  setup() {
+    let state = ref([]);
 
-}
+    onMounted(async () => {
+      state.value = await Cards();
+    });
+
+
+    return { state };
+  },
+};
 </script>
 
 <template>
-    <div>
       <div v-for="item in state" :key="item.id">
-            <h1>{{ item.spanishName }}</h1>
+            <!-- <h1>{{ item.spanishName }}</h1>
             <img :src="item.clowCard" alt="Clow Card" />
             <img :src="item.sakuraCard" alt="Sakura Card" />
             <p><strong>English Name:</strong> {{ item.englishName }}</p>
             <p><strong>Kanji:</strong> {{ item.kanji }}</p>
             <p><strong>Rōmaji:</strong> {{ item.Rōmaji }}</p>
             <p><strong>Appeared in Anime:</strong> {{ item.appeardAnime }}</p>
-            <p><strong>Meaning:</strong> {{ item.meaning }}</p>
+            <p><strong>Meaning:</strong> {{ item.meaning }}</p> -->
             <img :src="item.cardsReverse.clowReverse" alt="Clow Card Reverse" />
-            <img :src="item.cardsReverse.sakuraReverse" alt="Sakura Card Reverse" />
+            <!-- <img :src="item.cardsReverse.sakuraReverse" alt="Sakura Card Reverse" /> -->
         </div>
-    </div>
   </template>
   
