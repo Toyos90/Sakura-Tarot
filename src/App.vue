9dd943e35e557ce7c62990e2f52db7e5010923ce
HabibullahAfzali <hbafzali@gmail.com>
2023-07-05 09:26:56 +0200

    added

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@ import Cards from './components/Cards.vue';
 </script>
 
 <template>
-<<<<<<< HEAD
   <RouterLink to="/">Home</RouterLink>
-=======
-        <RouterLink to="/">Home</RouterLink>
-        <Cards/>
->>>>>>> f83a6fa322162e216c8150bca456adac11db356b
   <RouterView />
 </template>
 
